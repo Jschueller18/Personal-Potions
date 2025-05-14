e97c5e36ecd673cab9ba76f522fcde392cbc5a15
Jschueller18 <jschueller18@gmail.com>
2025-05-14 12:40:58 -0500

    proxy3 heroku

--- script.js
+++ script.js
@@ -287,7 +287,7 @@ document.addEventListener('DOMContentLoaded', function() {
             console.log('Sending data to API...');
             
             // Use CORS proxy to work around CORS issues with Vercel serverless functions
-            fetch('https://corsproxy.io/?https://personal-potions-api.vercel.app/api/customers/survey', {
+            fetch('https://cors-anywhere.herokuapp.com/https://personal-potions-api.vercel.app/api/customers/survey', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
